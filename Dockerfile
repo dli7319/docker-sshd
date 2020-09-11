2c1aeb159e33278798bd9a4444cd493b4ad75255
David Li <david@davidl.me>
2020-09-11 16:03:21 -0400

    Update dockerfile to allow remove connections to reverse tunneled ports.

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,10 @@ RUN apt install -y openssh-server rsync
 
 RUN echo 'root:m_root_pwd' | chpasswd
 RUN echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+RUN echo 'GatewayPorts clientspecified' >> /etc/ssh/sshd_config
 
 RUN mkdir -p /var/run/sshd
 
 RUN echo "Starting SSHD"
-EXPOSE 22 22350-22360
+EXPOSE 22 22350
 CMD ["/usr/sbin/sshd", "-D"]
--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build -t $IMAGE_NAME .
 docker run -d \
-           -p $SSH_PORT:22 \
-           -p $PROXY_PORT \
-           -v "/$(pwd)/.ssh:/root/.ssh" \
-	   -v "${DIR}/data:/root/data" \
+				   --restart unless-stopped \
+			     -p $SSH_PORT:22 \
+			     -p $PROXY_PORT:$PROXY_PORT \
+			     -v "/$(pwd)/.ssh:/root/.ssh" \
+				   -v "${DIR}/data:/root/data" \
            $IMAGE_NAME
-# ssh -L $PROXY_PORT:localhost:$PROXY_PORT root@localhost -p $SSH_PORT
